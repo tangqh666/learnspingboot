c6006260f58d143d16d789027b4bebf781232440
tt <fjfztqh@126.com>
2022-01-17 12:17:50 +0800

    2022-1-17  2.2节 init

---
+++ src/main/java/com/jiawa/wiki/LearnspingbootApplication.java
@@ -0,0 +1,13 @@
+package com.jiawa.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspingbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnspingbootApplication.class, args);
+    }
+
+}
